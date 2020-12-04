b47a0db9e80c4683c33fec8086d041de498a8fd5
CaliFlowers <72298949+CaliFlowers@users.noreply.github.com>
2020-12-04 14:24:57 -0800

    Add files via upload
    Further refining

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,52 +9,38 @@ with open(csvpath) as csvfile:
     lastmonthprofit=0
     totalprofit = 0
     totalchange = 0
-    greatestincrease = -999999          
-    greatestdecrease = 999999           
-    GImonth=""
-    GDmonth=""
+    greatestincrease = -999999
+    greatestdecrease = 999999
+    GImonth = ""
+    GDmonth = ""
     for line in csvreader:
         months+=1
         revenue = int(line[1])
-        currentmonth = str(line [0]) 
-        currentprofit = revenue      
+        currentmonth = str(line [0])
+        currentprofit = revenue
         monthlychange = revenue - lastmonthprofit
         totalprofit = totalprofit + revenue
-        astmonthprofit=revenue 
-        totalchange = round((totalchange + monthlychange),2) 
-# #with open(file_to_output, "w") as txt_file:
-# financial_summary = (
-#     f"\n\nElFinancial Analysis\n"
-#     f"xxxxxxxxxxxxxxxxxxxxxxxx\n"
-#     f"Total Months: {months}\n"
-#     f"-Total Profits: ${totalprofit}\n")
-#     print(financial_summary, end="")
-#     txt_file.write(financial_summary)
-
-    if monthlychange > greatestincrease:  
-
-            greatestincrease = monthlychange
-            GImonth = currentmonth 
-
-    elif monthlychange < greatestdecrease:
-
-           greatestdecrease = monthlychange
-           GDmonth = currentmonth
-           print(GImonth) 
-
-    average_change = round((totalchange/months),2) 
-    print(average_change)
+        astmonthprofit=revenue
+        totalchange = round((totalchange + monthlychange),2)
+        if monthlychange > greatestincrease:
+                greatestincrease = monthlychange
+                GImonth = currentmonth
+        elif monthlychange < greatestdecrease:
+              greatestdecrease = monthlychange
+              GDmonth = currentmonth
+              print(GImonth) 
+        average_change = round((totalchange/months),2) 
+        print(average_change)
 
     superlatives_summary = (
     f"\n\nElFinancial Analysis\n"
     f"xxxxxxxxxxxxxxxxxxxxxxxx\n"
     f"Total Months: {months}\n"
-    f"-Total Profits: ${totalprofit}\n"
+    f"Total Profits: ${totalprofit}\n"
     f"Average Change:  {average_change}\n"
     f"Greatest Increase in Profits:  {GImonth}: ${greatestincrease}\n"
     f"Greatest Decrease in Profits:  {GDmonth}: ${greatestdecrease}\n")
     print(superlatives_summary)
-
 
 with open(file_to_output, "w") as txt_file:
     txt_file.write(superlatives_summary)
